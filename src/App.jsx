79109d28e30a2b5e56c80d938b5bd96209057347
BerdiqulovN-dev <navruzbekturalov0333@gmail.com>
2023-08-19 14:41:18 +0500

    register page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Login from "./pages/Login";
+// import Login from "./pages/Login"; 
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-2xl font-semibold text-blue-700">App</h1>
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
     </div>
   );
 };
