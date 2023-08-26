964b37e4b140956ba83b5cfb16f4517f0323f95b
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-08-26 15:05:42 +0500

    brands section add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,10 @@ import ApplicationImg from "../assets/images/application.png";
 import CreditCardImg from "../assets/images/credit-card.png";
 import BooksImg from "../assets/images/books.png";
 import SofaImg from "../assets/images/sofa.png";
+import FirstBrand from "../assets/images/brand1.png"
+import SecondBrand from "../assets/images/brand2.png"
+import ThirdBrand from "../assets/images/brand3.png"
+
 
 const Home = () => {
   return (
@@ -90,7 +94,7 @@ const Home = () => {
         <div className="container mx-auto px-[90px]">
           <div className="services__inner py-[50px]">
             <ul className="grid grid-cols-3 gap-x-[30px]">
-              <li className="flex items-center border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
+              <li className="flex items-center transition-all hover:shadow-lg border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
                 <div className="card-body pl-8">
                   <h3 className="text-[22px] font-semibold mb-12">
                     Доставка по всему Узбекистану
@@ -101,7 +105,7 @@ const Home = () => {
                 </div>
                 <img src={DeliveryImage} alt="Delivery image" />
               </li>
-              <li className="flex  border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
+              <li className="flex transition-all hover:shadow-lg  border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
                 <div className="card-body pt-[25px] pl-8">
                   <h3 className="text-[22px] font-semibold mb-6">
                     Установите мобильное приложение
@@ -112,7 +116,7 @@ const Home = () => {
                 </div>
                 <img src={ApplicationImg} alt="Delivery image" />
               </li>
-              <li className="flex border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
+              <li className="flex transition-all hover:shadow-lg border-[0.5px] border-[#8D909B] border-opacity-50 rounded-[4px]">
                 <div className="card-body pt-[25px] pl-8">
                   <h3 className="text-[22px] font-semibold mb-6">
                     Оплата наличными или картой
@@ -206,6 +210,66 @@ const Home = () => {
                 <h4 className="text-[24px] text-[#273142] font-bold text-center">
                   Книги
                 </h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="brands mb-[70px]">
+        <div className="container mx-auto px-[90px]">
+          <div className="brands__inner">
+            <div className="brands-title pb-2 border-b mb-10 flex justify-between items-center">
+              <h2 className="text-[28px]  font-bold text-[#273142]">Бренды</h2>
+              <div className="actions flex gap-3">
+                <span className="left-arrow flex items-center justify-center p-1 bg-[#F4F7F8] hover:bg-[#e4f1f5] cursor-pointer rounded">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                  >
+                    <path
+                      d="M19.6747 22.2333L10.3247 15.9532L19.6747 9.7666V22.2333Z"
+                      fill="#8D909B"
+                    />
+                  </svg>
+                </span>
+                <span className="right-arrow flex items-center justify-center p-1 bg-[#F4F7F8] hover:bg-[#e4f1f5] cursor-pointer rounded">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                  >
+                    <path
+                      d="M12.3253 22.2333L21.6753 15.9532L12.3253 9.7666V22.2333Z"
+                      fill="#8D909B"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+            <ul className="grid grid-cols-6 justify-between">
+              <li className="flex items-center justify-center">
+                <img src={FirstBrand} alt="Brand image" />
+              </li>
+              <li className="flex items-center justify-center">
+                <img src={SecondBrand} alt="Brand image" />
+              </li>
+              <li className="flex items-center justify-center">
+                <img src={ThirdBrand} alt="Brand image" />
+              </li>
+              <li className="flex items-center justify-center">
+                <img src={FirstBrand} alt="Brand image" />
+              </li>
+              <li className="flex items-center justify-center">
+                <img src={SecondBrand} alt="Brand image" />
+              </li>
+              <li className="flex items-center justify-center">
+                <img src={ThirdBrand} alt="Brand image" />
               </li>
             </ul>
           </div>
