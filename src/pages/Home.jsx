33c13f4732457012689326e8f8e589ea32c8544b
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-08-26 12:30:05 +0500

    hero update and small banners add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,33 @@
-import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import banner1 from "../assets/images/banner-1.jpg";
-import banner2 from "../assets/images/banner-2.jpg";
-import banner3 from "../assets/images/banner-3.jpg";
-import banner4 from "../assets/images/banner-4.jpg";
-import MerosMerch from "../assets/images/Meros-Merch.svg";
-import MerosDiscont from "../assets/images/Meros-discont.svg";
-import MerosGift from "../assets/images/Meros-Gift.svg";
+
+
 import AsideImg from "../assets/images/aside-img.jpg";
 import Viewed from "../assets/images/visibility.svg";
 import Favotite from "../assets/images/favorite.svg";
 import Basket from "../assets/images/basket.svg";
 import Product1 from "../assets/images/product1.jpg";
+import SmallBanners from "../components/SmallBanners";
+import HeroBanners from "../components/HeroBanners";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="container mx-auto px-[90px]">
-        <section className="hero gap-3 grid grid-cols-[420px_minmax(420px,_1fr)_420px] grid-rows-[170px_minmax(170px,_1fr)_170px]">
-          <div className="col-start-1 col-end-3 row-start-1 row-end-3 ">
-            <Link to="/">
-              <img src={banner1} alt="" className="w-full h-full" />
-            </Link>
+      <section className="hero mb-[70px]">
+        <div className="container mx-auto px-[90px]">
+          <HeroBanners/>
+        </div>
+      </section>
+
+      <section className="specials">
+        <div className="container mx-auto px-[90px]">
+          <div className="specials__inner">
+            <h2 className="text-[28px] font-bold text-[#273142] mb-[30px]">Специально для вас</h2>
+            <SmallBanners/>
           </div>
-          <div className="col-start-3 col-end-4 row-start-1 row-end-2">
-            <Link to="/">
-              <img src={banner2} alt="" className="w-full h-full" />
-            </Link>
-          </div>
-          <div className=" col-start-3 col-end-4 row-start-2 row-end-3">
-            <Link to="/">
-              <img src={banner3} alt="" className="w-full h-full" />
-            </Link>
-          </div>
-          <div className="mt-6 col-start-1 col-end-4 row-start-3 row-end-4">
-            <Link to="/">
-              <img src={banner4} alt="" className="w-full h-full" />
-            </Link>
-          </div>
-        </section>
-        
-      </div>
+        </div>
+      </section>
 
       <Footer />
     </>
