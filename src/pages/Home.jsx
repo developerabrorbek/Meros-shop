954d4536a4bb515704b239dc77b4497027d84834
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-08-26 12:41:01 +0500

    Section title add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-
 import AsideImg from "../assets/images/aside-img.jpg";
 import Viewed from "../assets/images/visibility.svg";
 import Favotite from "../assets/images/favorite.svg";
@@ -9,6 +8,7 @@ import Basket from "../assets/images/basket.svg";
 import Product1 from "../assets/images/product1.jpg";
 import SmallBanners from "../components/SmallBanners";
 import HeroBanners from "../components/HeroBanners";
+import SectionTitle from "../components/SectionTitle";
 
 const Home = () => {
   return (
@@ -16,15 +16,23 @@ const Home = () => {
       <Header />
       <section className="hero mb-[70px]">
         <div className="container mx-auto px-[90px]">
-          <HeroBanners/>
+          <HeroBanners />
         </div>
       </section>
 
-      <section className="specials">
+      <section className="specials mb-[70px]">
         <div className="container mx-auto px-[90px]">
           <div className="specials__inner">
-            <h2 className="text-[28px] font-bold text-[#273142] mb-[30px]">Специально для вас</h2>
-            <SmallBanners/>
+            <SectionTitle title="Специально для вас" />
+            <SmallBanners />
+          </div>
+        </div>
+      </section>
+
+      <section className="offers">
+        <div className="container mx-auto px-[90px]">
+          <div className="offers__inner">
+            <SectionTitle title="Лучшие предложения!" />
           </div>
         </div>
       </section>
